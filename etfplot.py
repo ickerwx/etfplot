d4d53c9937d9a80236747c7fc570ee62db878f76
René Werner <mail@renewerner.net>
2020-07-17 18:53:20 +0200

    you can never have enough lines

--- etfplot.py
+++ etfplot.py
@@ -35,10 +35,13 @@ def main():
                     data[ticker].append((date, float(price.replace(',', '.'))))
                 else:
                     data[ticker] = [(date, float(price.replace(',', '.')))]
-    fig, ax = pl.subplots()
     if len(data.keys()) == 0:
         print(f'No ticker data found in file {filepath}.')
         sys.exit(3)
+    fig, ax = pl.subplots()
+    ax.yaxis.set_minor_locator(AutoMinorLocator(10))  # have ten minor ticks between two major ticks
+    ax.grid(axis='y', which='major', color='#dddddd', linestyle='--')  # major tick lines are dashed
+    ax.grid(axis='y', which='minor', color='#dddddd', linestyle=':')  # minor tick lines are dotted
     for ticker in data.keys():
         xticks, y = map(list, zip(*data[ticker]))
         x = [datetime.datetime.strptime(s, "%d.%m.%y").timestamp() for s in xticks]
@@ -46,11 +49,12 @@ def main():
         pl.setp(ax.get_xticklabels(), rotation=45, horizontalalignment='right', fontsize='x-small')  # rotate x axis labels by 45 degrees
         pl.xticks(x, xticks)  # set x tick labels, x is a list of unix timestamps, xticks is a list of readable dates
         ax.scatter(x, y, label=f'{ticker} - {description[ticker]}')  # create a scatter plot, plot only dots
-        ax.plot(x, y)  # create a line chart, connect the dots
-        ax.legend(frameon=False)  # no frame around the legend
-        ax.yaxis.set_minor_locator(AutoMinorLocator(10))  # have ten minor ticks between two major ticks
-        ax.grid(axis='y', which='major', color='#dddddd', linestyle='--')  # major tick lines are dashed
-        ax.grid(axis='y', which='minor', color='#dddddd', linestyle=':')  # minor tick lines are dotted
+        a = ax.plot(x, y)  # create a line chart, connect the dots
+        ax.axhline(y[0], color=a[0].get_color(), ls='--', lw=.5)  # draw a horizontal line at the first known value to mark the starting value
+    for k in x:
+        ax.axvline(k, color='black', ls=':', lw=0.5)  # draw vertical lines at each date
+
+    ax.legend(frameon=False)  # no frame around the legend
     mplcursors.cursor(hover=True)
     pl.show()
 
